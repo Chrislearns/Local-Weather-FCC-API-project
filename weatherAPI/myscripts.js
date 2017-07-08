76fe110371bd463817aac91d070d72dec6b08f91
Chris Drain <DrainC4@yahoo.com>
2017-07-07 18:24:48 -0700

    got CORS working javascript must be redone

--- weatherAPI/myscripts.js
+++ weatherAPI/myscripts.js
@@ -10,34 +10,32 @@ console.log(position);
     $.ajax({
       url: "https://api.darksky.net/forecast/b723c0de034cb7462e8e73bc266af457/" + position.coords.latitude + "," + position.coords.longitude ,
       type: 'GET',
-      dataType: 'json',
+      dataType: 'jsonp',
+
       success: function(json) {
 console.log(json);
-       var descrip = json.weather[0].description;
-    
+       var descrip = json.currently.apparentTemperature;
+console.log(descrip);
    $(".info").html((descrip.replace(/\w\S*/g, function(c) {
           return c.charAt(0).toUpperCase() + c.substr(1).toLowerCase();
         })) + " " + "<img class = newicon src=" + "http://openweathermap.org/img/w/" + json.weather[0].icon + ".png></img>");
         $(".location").html(json.name + ", " + json.sys.country);
 
 
-    var convertF = "<button class = btn btn-success btn-transparent  change-temp style = color:red>ºF</button>";    
+    var convertF = "<button class = btn btn-success btn-transparent  change-temp style = color:red>ºF</button>";
   var convertC = "<button class = btn btn-success btn-transparent  change-temp style = color:red> ºC</button>";
-  
+
    var degrees = json.main.temp;
        var farenheit = Math.round(1.8 *(degrees - 273) + 32);
              var celsius = Math.round((farenheit - 32) / 1.8);
   $(".degrees").html(farenheit + convertF);
-      
-       
- 
-     }     
-  
-          
+
+
+
+     }
+
+
       });
     });
- 
-  });
 
-      
-          
+  });
